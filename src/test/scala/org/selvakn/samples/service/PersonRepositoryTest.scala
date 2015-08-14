8e0328b9d4a0af69494becd00771fb344d518e91
Selva <k.n.selvakumar@gmail.com>
2015-08-14 10:14:09 +0530

    Removing NoArgsConstructor

--- src/test/scala/org/selvakn/samples/service/PersonRepositoryTest.scala
+++ src/test/scala/org/selvakn/samples/service/PersonRepositoryTest.scala
@@ -1,19 +1,7 @@
 package org.selvakn.samples.service
 
-import javax.persistence.{EntityManager, EntityManagerFactory}
-
-import org.junit.runner.RunWith
-import org.scalatest.{BeforeAndAfterEach, FunSpec}
-import org.scalatest.junit.JUnitRunner
-import org.selvakn.samples.{ApplicationContextProvider, Config}
 import org.selvakn.samples.models.Person
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.SpringApplicationContextLoader
-import org.springframework.context.ApplicationContext
-import org.springframework.orm.jpa.JpaTransactionManager
-import org.springframework.test.context.{ContextConfiguration, TestContextManager}
-import org.springframework.transaction.{TransactionStatus, TransactionDefinition}
-import org.springframework.transaction.support.DefaultTransactionDefinition
 
 import scala.collection.JavaConverters._
 
